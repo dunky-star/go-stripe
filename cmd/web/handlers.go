fa443a7d73d9639193db50ef511c3c3e0ac6a11e
Geoffrey Duncan Opiyo <dunkygeoffrey39@gmail.com>
2026-02-22 13:59:32 -0700

    feature(Backend): Tested successful payment with test cards-4

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,9 +1,47 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
 
 func (app *application) VirtualCardHandler(w http.ResponseWriter, r *http.Request) {
 	if err := app.renderTemplate(w, r, "terminal", nil); err != nil {
+		app.errorLog.Println(err)
+	}
+}
+
+func (app *application) PaymentSucceededHandler(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		app.errorLog.Println(err)
+		return
+	}
+
+	// Read the posted data
+	paymentAmountStr := r.Form.Get("payment_amount")
+	paymentCurrency := r.Form.Get("payment_currency")
+	paymentIntent := r.Form.Get("payment_intent")
+	paymentMethod := r.Form.Get("payment_method")
+	cardHolder := r.Form.Get("cardholder_name")
+	cardholderEmail := r.Form.Get("cardholder_email")
+
+	// Convert cents to dollars
+	paymentAmountCents, _ := strconv.Atoi(paymentAmountStr)
+	paymentAmount := fmt.Sprintf("%.2f", float64(paymentAmountCents)/100.0)
+
+	data := make(map[string]interface{})
+	data["cardholder"] = cardHolder
+	data["cardholder_email"] = cardholderEmail
+	data["payment_amount"] = paymentAmount
+	data["payment_currency"] = paymentCurrency
+	data["payment_intent"] = paymentIntent
+	data["payment_method"] = paymentMethod
+
+	if err := app.renderTemplate(w, r, "payment-succeeded", &templateData{
+		Data: data,
+	}); err != nil {
 		app.errorLog.Println(err)
 	}
 }
