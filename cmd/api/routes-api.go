5635045dfc0436d018d9180391d4d4ae1649d9fa
Geoffrey Duncan Opiyo <dunkygeoffrey39@gmail.com>
2026-05-02 15:06:28 -0700

    feat: Login and Logout functionalities

--- cmd/api/routes-api.go
+++ cmd/api/routes-api.go
@@ -9,6 +9,7 @@ func (app *application) routes() http.Handler {
 	mux.HandleFunc("GET /api/v1/widget/{id}", app.GetWidgetByID)
 	mux.HandleFunc("POST /api/v1/create-customer-and-subscribe-to-plan", app.CreateCustomerAndSubscribeToPlan)
 	mux.HandleFunc("POST /api/v1/authenticate", app.CreateAuthToken)
+	mux.HandleFunc("POST /api/v1/is-authenticated", app.CheckAuthentication)
 
 	return app.enableCORS(mux)
 }
