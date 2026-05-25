4e7a0d32f9f2d805dc5c3953e0ce48c951782cd1
Geoffrey Duncan Opiyo <dunkygeoffrey39@gmail.com>
2026-05-25 13:10:45 -0700

    Email sending

--- cmd/customer-invoice/invoice-handlers.go
+++ cmd/customer-invoice/invoice-handlers.go
@@ -1,10 +1,30 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+	"time"
 )
 
+type Order struct {
+	ID        int       `json:"id"`
+	Quantity  int       `json:"quantity"`
+	Amount    int       `json:"amount"`
+	Product   string    `json:"product"`
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
 func (app *application) CreateAndSendInvoice(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %s", "Create and send invoice")
+	// fmt.Fprintf(w, "Hello, %s", "Create and send invoice")
+	// recieve json
+
+	// generate a pdf invoice
+
+	// create mail
+
+	// send mail with attachment
+
+	// send response
 }
