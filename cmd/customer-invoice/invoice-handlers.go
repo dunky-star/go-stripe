77031f7905294979da4464977b956a85f2c2fe3e
Geoffrey Duncan Opiyo <dunkygeoffrey39@gmail.com>
2026-05-26 23:14:21 -0700

    Feat: Emailing invoice

--- cmd/customer-invoice/invoice-handlers.go
+++ cmd/customer-invoice/invoice-handlers.go
@@ -39,7 +39,7 @@ func (app *application) CreateAndSendInvoice(w http.ResponseWriter, r *http.Requ
 		fmt.Sprintf("./invoices/%d.pdf", order.ID),
 	}
 
-	err = app.SendMail("info@widgets.com", order.Email, "Your invoice", "invoice", attachments, nil)
+	err = app.SendMail("info@dunkystar.com", order.Email, "Your invoice", "invoice", attachments, nil)
 	if err != nil {
 		app.badRequest(w, r, err)
 		return
